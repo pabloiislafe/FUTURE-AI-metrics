229fa364b9205c2a01fa06d0f947a2f8f32235c7
pabloiislafe <pablo.rodriguez.iislafe@gmail.com>
2025-11-14 15:57:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-from setuptools.command.develop import develop
-
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="future_ai_metrics",  
+    name="Metrics",                  # nombre del paquete
     version="0.1.0",
-    description="A lightweight evaluation toolkit for AI model assessment (based on FUTURE-AI principles)",
+    description="Evaluation toolkit for AI model assessment",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="FUTURE-AI consortium",
-    author_email="parodbe@gmail.com",
+    author_email="leonor_cerda@iislafe.es",
     url="https://github.com/pabloiislafe/FUTURE-AI-metrics",
-    packages=find_packages(),
+    packages=["Metrics", "Metrics.Robustness"],   # 👈 INSTALA SOLO Metrics
     include_package_data=True,
-    license="GNU")
+    license="GNU"
+)
